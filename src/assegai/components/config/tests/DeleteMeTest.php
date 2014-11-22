24797761c51cd966debf79c88a5edca5731a4acc
Etenil <boss@etenil.net>
2014-11-22 15:57:38 +0000

    Working tests
    Added an embryo of test just to ensure it works, and adapted the test
    configs accordingly.

---
+++ src/assegai/components/config/tests/DeleteMeTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace assegai\components\config\tests;
+
+class DeleteMeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testThing()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+    }
+}
